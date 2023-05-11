93da9e74b898d2c51575fab004d18512892194f1
PalisseVolia <palissev@gmail.com>
2023-05-11 23:39:37 +0200

    test

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <thread>
 #include <iostream>
+#include <windows.h>
 #include <filesystem>
 
 using namespace std;
@@ -15,6 +16,10 @@ static vector<string> TabFilesOneOld;
 static vector<string> TabFilesTwoOld;
 static vector<string> TabFilesOne;
 static vector<string> TabFilesTwo;
+static vector<string> TabLastModifiedOneOld;
+static vector<string> TabLastModifiedTwoOld;
+static vector<string> TabLastModifiedOne;
+static vector<string> TabLastModifiedTwo;
 
 // Path des dossiers à synchroniser
 static fs::path dirPathOne("C:/Users/vpali/Desktop/TEST");
@@ -24,7 +29,7 @@ static fs::path dirPathTwo("C:/Users/vpali/Desktop/DESTINATION");
 void init();
 void Sync(int Indicator);
 int CompareVersion();
-string GetDirFilePaths(const fs::path &dirPath, vector<string> &TabFiles);
+string GetDirFilePaths(const fs::path &dirPath, vector<string> &TabFiles, vector<string> &TabLastModified);
 
 int main()
 {
@@ -38,6 +43,35 @@ int main()
         Sync(CompareVersion());
     }
 
+    // CompareVersion();
+
+    // cout << "TabLastModifiedOne" << endl;
+    // for (int i = 0; i < TabLastModifiedOne.size(); i++)
+    // {
+    //     cout << TabLastModifiedOne[i] << endl;
+    // }
+    // cout << "TabLastModifiedTwo" << endl;
+    // for (int i = 0; i < TabLastModifiedTwo.size(); i++)
+    // {
+    //     cout << TabLastModifiedTwo[i] << endl;
+    // }
+
+    // string temp;
+    // cin >> temp;
+
+    // CompareVersion();
+
+    // cout << "TabLastModifiedOne" << endl;
+    // for (int i = 0; i < TabLastModifiedOne.size(); i++)
+    // {
+    //     cout << TabLastModifiedOne[i] << endl;
+    // }
+    // cout << "TabLastModifiedTwo" << endl;
+    // for (int i = 0; i < TabLastModifiedTwo.size(); i++)
+    // {
+    //     cout << TabLastModifiedTwo[i] << endl;
+    // }
+
     return 0;
 }
 
@@ -47,11 +81,15 @@ void init()
     // Initialisation des tableaux de path fichiers
     TabFilesOneOld.clear();
     TabFilesOne.clear();
-    GetDirFilePaths(dirPathOne, TabFilesOne);
+    TabLastModifiedOneOld.clear();
+    TabLastModifiedOne.clear();
+    GetDirFilePaths(dirPathOne, TabFilesOne, TabLastModifiedOne);
 
     TabFilesTwoOld.clear();
     TabFilesTwo.clear();
-    GetDirFilePaths(dirPathTwo, TabFilesTwo);
+    TabLastModifiedTwoOld.clear();
+    TabLastModifiedTwo.clear();
+    GetDirFilePaths(dirPathTwo, TabFilesTwo, TabLastModifiedTwo);
 
     // Dossier 1 à la priorité à l'initialisation
     Sync(1);
@@ -79,12 +117,22 @@ int CompareVersion()
     TabFilesOneOld.clear();
     TabFilesOneOld = TabFilesOne;
     TabFilesOne.clear();
-    GetDirFilePaths(dirPathOne, TabFilesOne);
+
+    TabLastModifiedOneOld.clear();
+    TabLastModifiedOneOld = TabLastModifiedOne;
+    TabLastModifiedOne.clear();
+
+    GetDirFilePaths(dirPathOne, TabFilesOne, TabLastModifiedOne);
 
     TabFilesTwoOld.clear();
     TabFilesTwoOld = TabFilesTwo;
     TabFilesTwo.clear();
-    GetDirFilePaths(dirPathTwo, TabFilesTwo);
+
+    TabLastModifiedTwoOld.clear();
+    TabLastModifiedTwoOld = TabLastModifiedTwo;
+    TabLastModifiedTwo.clear();
+
+    GetDirFilePaths(dirPathTwo, TabFilesTwo, TabLastModifiedTwo);
 
     if (TabFilesOne.size() != TabFilesOneOld.size())
     {
@@ -100,10 +148,18 @@ int CompareVersion()
         {
             return 1;
         }
+        if (TabLastModifiedOne[i] != TabLastModifiedOneOld[i])
+        {
+            return 1;
+        }
     }
     for (int i = 0; i < TabFilesTwo.size(); i++)
     {
         if (TabFilesTwo[i] != TabFilesTwoOld[i])
+        {
+            return 2;
+        }
+        if (TabLastModifiedTwo[i] != TabLastModifiedTwoOld[i])
         {
             return 2;
         }
@@ -112,8 +168,8 @@ int CompareVersion()
     return 0;
 }
 
-// Récupération des path des fichiers d'un dossier
-string GetDirFilePaths(const fs::path &dirPath, vector<string> &TabFiles)
+// Récupération des path des fichiers d'un dossier et de leur date de mdoification
+string GetDirFilePaths(const fs::path &dirPath, vector<string> &TabFiles, vector<string> &TabLastModified)
 {
     if (!fs::exists(dirPath))
     {
@@ -129,8 +185,35 @@ string GetDirFilePaths(const fs::path &dirPath, vector<string> &TabFiles)
     {
         if (fs::is_regular_file(entry.path()))
         {
+            // Path des fichiers
             string pathtemp = entry.path().string().substr(entry.path().string().find(dirPath.string()) + dirPath.string().size() + 1);
             TabFiles.push_back(pathtemp);
+
+            // Date de dernière modification des fichiers
+            const std::wstring filePathWide = std::wstring(entry.path().wstring());
+            HANDLE fileHandle = CreateFileW(filePathWide.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+            if (fileHandle != INVALID_HANDLE_VALUE)
+            {
+                FILETIME lastWriteTime;
+                if (GetFileTime(fileHandle, NULL, NULL, &lastWriteTime))
+                {
+                    SYSTEMTIME systemTime;
+                    if (FileTimeToSystemTime(&lastWriteTime, &systemTime))
+                    {
+                        TabLastModified.push_back(to_string(systemTime.wYear) + "-" + to_string(systemTime.wMonth) + "-" + to_string(systemTime.wDay) + " " + to_string(systemTime.wHour) + ":" + to_string(systemTime.wMinute) + ":" + to_string(systemTime.wSecond));
+                    }
+                }
+                else
+                {
+                    return ("ERREUR : Impossible de récupérer la date de dernière modification du fichier : " + entry.path().string());
+                }
+
+                CloseHandle(fileHandle);
+            }
+            else
+            {
+                return ("ERREUR : Impossible d'ouvrir le fichier : " + entry.path().string());
+            }
         }
     }
 
